39d1b47484abb5c23f04611daa3965a67ee01df2
Tim Horist <TimelikeClosure@users.noreply.github.com>
2017-03-01 19:10:24 -0800

    completed stack level 2 basic requirements

--- 05. Stacks/Level 2/stack-L2.js
+++ 05. Stacks/Level 2/stack-L2.js
@@ -14,13 +14,13 @@
    double stack is a data structure that allows you to push and pop values from both
    the front and the back of the stack.
 
-    [ ] Complete the implementation of a DoubleStack class
+    [x] Complete the implementation of a DoubleStack class
     [x] .storage property to hold the items on the stack using a standard array
-    [ ] .push() function to push a value from the back
-    [ ] .pop() function to push a value from the back
-    [ ] .pushFront() function to push a value from the front
-    [ ] .popFront() function to push a value from the front
-    [ ] .length property to return the total size used
+    [x] .push() function to push a value from the back
+    [x] .pop() function to push a value from the back
+    [x] .pushFront() function to push a value from the front
+    [x] .popFront() function to push a value from the front
+    [x] .length property to return the total size used
 
   NOTE: Do not use any built-in features
   NOTE: Do not focus on edge cases or error conditions
@@ -40,17 +40,57 @@ var DoubleStack = function(initialCapacity) {
 };
 
 DoubleStack.prototype.push = function(value) {
-  // ...
+  this.storage[this.storage.length - ++this.length - this.lengthFront] = value;
 };
 
 DoubleStack.prototype.pop = function() {
-  // ...
+  return this.storage[this.storage.length - (this.length--) - this.lengthFront];
 };
 
-DoubleStack.prototype.pushFront = function(value) {
-  // ...
+DoubleStack.prototype.pushFront = function (value) {
+  this.length++;
+  this.storage[this.lengthFront++] = value;
 };
 
-DoubleStack.prototype.popFront = function() {
-  // ...
+DoubleStack.prototype.popFront = function () {
+  this.length--;
+  return this.storage[--this.lengthFront];
 };
+
+//  TESTS
+
+var ds = new DoubleStack(8);
+function dsTest(array, length, lengthFront) {
+  return array.reduce(function (last, value, index) {
+    return (value === undefined)
+      ? (last)
+      : (last && value === ds.storage[index]);
+  }, true)
+    && length === ds.length
+    && lengthFront === ds.lengthFront;
+}
+
+// .push() should grow the back stack toward the center
+
+ds.push(1);
+ds.push(2);
+ds.push(3);
+
+console.log('.push() test passed: ' + dsTest([, , , , , 3, 2, 1], 3, 0));
+
+// .pop() should shrink the back stack front the center
+
+console.log('.pop() test passed: ' + ((ds.pop() === 3) && dsTest([, , , , , , 2, 1], 2, 0)));
+
+// .pushFront() should grow the front stack toward the center
+
+
+ds.pushFront(4);
+ds.pushFront(5);
+ds.pushFront(6);
+
+console.log('.pushFront() test passed: ' + dsTest([4, 5, 6, , , , 2, 1], 5, 3));
+
+// .popFront() should shrink the front stack from the center
+
+console.log('.popFront() test passed: ' + ((ds.popFront() === 6) && dsTest([4, 5, , , , , 2, 1], 4, 2)));
