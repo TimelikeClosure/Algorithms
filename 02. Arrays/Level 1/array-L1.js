3e066a8d57c85a90c4bf016436da1b27ae5fc2b6
Tim Horist <TimelikeClosure@users.noreply.github.com>
2017-03-03 19:07:05 -0800

    completed arrays level 1

--- 02. Arrays/Level 1/array-L1.js
+++ 02. Arrays/Level 1/array-L1.js
@@ -54,28 +54,31 @@ if(!Array.prototype.equals) {
    being built into nearly every programming language.  The following exercises should
    serve as a glorious warm-up to the next few topics.
 
-    [ ] Fill any array with value, zero if none provided, return original array
-    [ ] Return the sum of all values in any array
-    [ ] Return the average value of any array
-    [ ] Return the median of any array that has both an odd and even number of values
-    [ ] Return the index the value specified, or null if not found
-    [ ] Return index of nth last odd in any array, 1 being the fist, etc., null = not found
-      [ ] Use only a single iteration
-    [ ] Select a range of values in any array, return the average of the values of the range
-    [ ] Copy contents of one array to another.  Do not use any built-in functions.
-    [ ] Swap two elements in an array.  Return the same array passed in.  Do not use any built-in functions.
+    [x] Fill any array with value, zero if none provided, return original array
+    [x] Return the sum of all values in any array
+    [x] Return the average value of any array
+    [x] Return the median of any array that has both an odd and even number of values
+    [x] Return the index the value specified, or null if not found
+    [x] Return index of nth last odd in any array, 1 being the fist, etc., null = not found
+      [x] Use only a single iteration
+    [x] Select a range of values in any array, return the average of the values of the range
+    [x] Copy contents of one array to another.  Do not use any built-in functions.
+    [x] Swap two elements in an array.  Return the same array passed in.  Do not use any built-in functions.
 
   NOTE: Satisfy all the tests.  Do not modify or comment of them out.
 
 */
 
 
-// [ ] Fill any array with value, zero if none provided, return original array
+// [x] Fill any array with value, zero if none provided, return original array
 //
 var z = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 function fillArray(a, value) {
-  // fill it
+  for (let i = 0, len = a.length; i < len; i++){
+    a[i] = value || 0;
+  }
+  return a;
 }
 
 console.log("fillArray...");
@@ -83,38 +86,48 @@ console.log("  fillArray(z) should be all zeros: " + (fillArray(z).equals([0, 0,
 console.log("  fillArray(z, 5) should be all 5's: " + (fillArray(z, 5).equals([5, 5, 5, 5, 5, 5, 5, 5, 5, 5])));
 
 
-// [ ] Return the sum of all values in any array
+// [x] Return the sum of all values in any array
 //
 var a = [1, 2, 3, 4, 5, 6, 7, 8];
 
 function sum(a) {
-  // Return sum
+  for (var i = 0, sum = 0, len = a.length; i < len; i++){
+    sum += a[i];
+  }
+  return sum;
 }
 
 console.log("sum...");
 console.log("  sum(a) should be 36: " + (sum(a) === 36));
 
 
-// [ ] Return the average value of any array
+// [x] Return the average value of any array
 //
 var a1 = [23, 17, 23, 42, 8, 2, 73, 101, 83, 92];
 
 function average(a) {
-  // return average
+  for (var i = 0, sum = 0, len = a.length; i < len; i++){
+    sum += a[i];
+  }
+  return sum / len;
 }
 
 console.log("average...");
 console.log("  average(a1) should be 46.4: " + (average(a1) === 46.4));
 
 
-//  [ ] Return the median of any array that has both an odd and even number of values
+//  [x] Return the median of any array that has both an odd and even number of values
 //
 var a2 = [1,2,4,4,6,7,8,9,12];
 var a3 = [4,5,9,10,11,15,22,20,21,21];
 var a0 = [10.1, 9.99, 12.32, 17.86, 9.1, 0];
 
 function median(a) {
-  // return median
+  var half = a.length / 2;
+  if (Number.isInteger(half)) {
+    return (a[half] + a[half-1])/2;
+  }
+  return a[Math.floor(half)];
 }
 
 console.log("median...");
@@ -123,25 +136,35 @@ console.log("  median(a3) should be 13: " + (median(a3) === 13));
 console.log("  median(a0) should be 15.09: " + (median(a0) === 15.09));
 
 
-// [ ] Return the index the value specified, or null if not found
+// [x] Return the index the value specified, or null if not found
 //
 var a4 = ["zero", "one", "two", "three", "four", "five"];
 
 function findIndex(a, value) {
-  // return index or null
+  for (let i = 0, len = a.length; i < len; i++){
+    if (a[i] === value) {
+      return i;
+    }
+  }
+  return null;
 }
 
 console.log("findIndex...");
 console.log("  findIndex('three') should be 3: " + (findIndex(a4, "three") === 3));
 
 
-// [ ] Return index of nth last odd in any array, 1 being the fist, etc., null = not found
-// [ ] Use only a single iteration
+// [x] Return index of nth last odd in any array, 1 being the fist, etc., null = not found
+// [x] Use only a single iteration
 //
 var a5 = [4, 3, 8, 8, 6, 9, 10, 12, 10, 9, 0, 5, 16, 2];
 
 function findNthLastOdd(a, n) {
-  // return nth last add
+  for (let i = a.length - 1; i >= 0; i--){
+    if (a[i] % 2 && !(--n)){
+      return i;
+    }
+  }
+  return null;
 }
 
 console.log("findNthLastOdd...");
@@ -150,25 +173,31 @@ console.log("  findNthLastOdd(a5, 2) should be 9: " + (findNthLastOdd(a5, 2) ===
 console.log("  findNthLastOdd(a5, 4) should be 1: " + (findNthLastOdd(a5, 4) === 1));
 
 
-// [ ] Select a range of values in any array, return the average of the values of the range
+// [x] Select a range of values in any array, return the average of the values of the range
 //
 var a6 = [0, 1, 2, 3, 4, 5, 10, 15, 23, 54, 22, 1, 8, 4, 2, 2, 2, 0, 1];
 
 function getAverageOfRange(a, start, end) {
-  // return average of values selected from a subarray
+  for (var min = Math.max(start, 0), i = min, sum = 0, max = Math.min(end + 1, a.length); i < max; i++){
+    sum += a[i];
+  }
+  return sum / (max - min);
 }
 
 console.log("getAverageOfRange...");
 console.log("  getAverageOfRange(a6, 5, 9) should be 21.4: " + (getAverageOfRange(a6, 5, 9) === 21.4));
 
 
-// [ ] Copy contents of one array to another.  Do not use any built-in functions.
+// [x] Copy contents of one array to another.  Do not use any built-in functions.
 //
 var a7 = [0, 10, 20, 30, 35, 55, 75, 100];
 var b7 = [];
 
 function copyArray(source, target) {
-  // return copy
+  for (let i = 0, len = source.length; i < len; i++){
+    target[i] = source[i];
+  }
+  return target;
 }
 
 console.log("copyArray...");
@@ -177,12 +206,19 @@ console.log("  b7 should not be empty: " + (b7.length > 0));
 console.log("  b7 should be same as a7: " + (b7.equals(a7)));
 
 
-// [ ] Swap two elements in an array.  Return the same array passed in.  Do not use any built-in functions.
+// [x] Swap two elements in an array.  Return the same array passed in.  Do not use any built-in functions.
 //
 var a8 = [1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37];
 
 function swap(a, indexSource, indexTarget) {
-  // swap 'em
+  // alternate XOR swap
+  // a[indexTarget] = a[indexTarget] ^ a[indexSource];
+  // a[indexSource] = a[indexTarget] ^ a[indexSource];
+  // a[indexTarget] = a[indexTarget] ^ a[indexSource];
+  const temp = a[indexTarget];
+  a[indexTarget] = a[indexSource];
+  a[indexSource] = temp;
+  return a;
 }
 
 console.log("swap...");
